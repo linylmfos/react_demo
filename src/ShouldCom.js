cc8e32ecad125ea557210ad8a082947eef1a1fb1
linylmfos <iqianbaidu@163.com>
2020-01-28 11:36:55 +0800

    shouldComponentUpdate 3

--- src/ShouldCom.js
+++ src/ShouldCom.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 class Parent extends React.Component {
     constructor(props) {
         super(props);
@@ -35,22 +36,49 @@ class Child extends React.Component {
         super(props);
 
         this.state = {
-            nama: '',
+            name: '',
             age: ''
         }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
         console.log(nextProps, nextState);
-        return true
+        return (
+            nextProps.id !== this.props.id || nextState.name !== this.state.name
+        )
+        // return true
     }
 
     loadData(id) {
         console.log('id',id);
-        fetch(`data/data${id}.txt`).then(res => {
-            console.log('ddd', res)
+        fetch(`data/data${id}.json`, {
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(res => {
             res.json().then(data => {
+                console.log('d', data)
                 this.setState(data);
+            }).catch(err => {
+            }).finally(() => {
+                var data;
+                if(id === 1) {
+                    data = {
+                        "name": "张三",
+                        "age": 18
+                    }
+                    this.setState(data);
+                } else if(id ===2){
+                    data = {
+                        "name": "张三丰", "age": "20"
+                    }
+                    this.setState(data);
+                } else if(id === 3) {
+                    data = {
+                        "name": "张丰", "age": "22"
+                    }
+                    this.setState(data);
+                }
             })
         })
     }
@@ -69,7 +97,7 @@ class Child extends React.Component {
         return (
             <div>
                 <h2>ID: {this.props.id}</h2>
-                <p>用户名： {this.state.name}-年龄： {this.state.age}</p>
+                <p>用户名： {this.state.name}<br/> 年龄： {this.state.age}</p>
             </div>
         )
     }
